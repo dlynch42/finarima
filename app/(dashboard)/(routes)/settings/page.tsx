5111083b34ca05fdfa2aec2862ea5772739c0201
Devin Lynch <djlynch17@gmail.com>
2023-10-11 12:00:18 -0700

    Updated pages

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -1,7 +1,22 @@
+import { HardHat } from "lucide-react";
+
 const SettingsPage = () => {
     return (
-        <div>
-            <h1>TODO</h1>
+        <div className="h-screen flex justify-center items-center">
+            <div className="w-full h-full grid grid-cols-3 gap-4 content-center">
+                <div className="flex flex-col justify-center items-center">
+                </div>
+                <div className="flex flex-col justify-center items-center justify-self-center">
+                    <div className="mt-auto mb-auto">
+                        <h1 className="text-center">
+                            <HardHat size={100}/>
+                        </h1>
+                        <h1 className="text-center">
+                            Coming Soon!
+                        </h1>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
