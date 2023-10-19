0ff8100b31c3fb5b8f2e07aecd57fd04c8f46150
Devin Lynch <djlynch17@gmail.com>
2023-10-19 10:05:17 -0700

    Finished project

--- components/loader.tsx
+++ components/loader.tsx
@@ -2,7 +2,8 @@ import { RotateCw } from 'lucide-react';
 
 export const Loader = () => {
     return (
-        <div className="h-full flex flex-col gap-y-4 items-center justify-center">
+        <div className="h-full flex flex-col gap-y-4 items-center justify-center text-sm text-muted-foreground">
+            Your content may take up to 20 seconds to load
             <div className="w-10 h-10 relative animate-spin">
                 <RotateCw className="h-10 w-10 bg[#111827]"/>
             </div>
