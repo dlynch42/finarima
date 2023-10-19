8a13e53563b1ad2aab0e6bd9f57433aa3b3a3254
Devin Lynch <djlynch17@gmail.com>
2023-10-19 16:13:17 -0700

    Added project id

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'o1y9dq',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
